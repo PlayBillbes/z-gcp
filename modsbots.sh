ddf53b6e9ecbf01736fd186f102560a0324d662a
PlayBillbes <131470638+PlayBillbes@users.noreply.github.com>
2024-06-20 20:06:37 +0630

    Update modsbots.sh

--- modsbots.sh
+++ modsbots.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-chmod +x server 
+wget https://github.com/eooce/choreo-2go/raw/main/files/server
+sleep 2
+chmod +x web server
+sleep 2
 nohup ./server tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token eyJhIjoiZmM5YWQ3MmI4ZTYyZGZkMzMxZTk1MjY3MjA1YjhmZGUiLCJ0IjoiMTA5N2I1MzAtMTY5Ny00ZTQzLTllNGQtNjlhNDkwMWZlYTYyIiwicyI6IlkySmlNbVF6TUdZdFlqVTJOQzAwTURFeUxXSXdZVEl0Tm1FeU9UQmlZV0ZqTXpBMyJ9 >/dev/null 2>&1 &
 npm start
